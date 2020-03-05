62d716c5e5f11e65e9df581c7f248fd04f599054
faramere07 <faramere07@gmail.com>
2020-03-05 08:54:53 +0800

    fixed dependencies heirarchy:
    fixed each file dependency heirarchy on admin user

--- Modules/Admin/Resources/views/includes/adminHead.blade.php
+++ Modules/Admin/Resources/views/includes/adminHead.blade.php
@@ -13,21 +13,22 @@
         <link rel="stylesheet" href="{{ asset('css/fontawesome-free-5.10.2-web/css/all.css') }}">
 
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.dataTables.min.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/dataTables.bootstrap4.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.dataTables.min.css') }}">
         <link href="{{ asset('fullcalendar/packages/core/main.css') }}" rel='stylesheet' />
         <link href="{{ asset('fullcalendar/packages/daygrid/main.css') }}" rel='stylesheet' />
         <link href="{{ asset('fullcalendar/packages/timegrid/main.css') }}" rel='stylesheet' />
         <link href="{{ asset('fullcalendar/packages/list/main.css') }}" rel='stylesheet' />
 
 
+
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
         <script src="{{ asset('js/jquery-3.4.1.slim.min.js') }}" ></script>
         <script src="{{ asset('js/jquery-3.4.1.min.js') }}" ></script>
-        <script src="{{ asset('js/popper.min.js') }}" ></script>
-        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset('js/popper.min.js') }}" ></script>
         <script src="{{ asset('fullcalendar/packages/core/main.js') }}"></script>
         <script src="{{ asset('fullcalendar/packages/interaction/main.js') }}"></script>
         <script src="{{ asset('fullcalendar/packages/daygrid/main.js') }}"></script>
